ddf55ceea2e83f86c524c525df62dcc82b37afff
Omar Ibrahim <oielbanna@gmail.com>
2020-01-29 22:35:34 -0500

    added headers

--- Project1/main.py
+++ Project1/main.py
@@ -1,8 +1,13 @@
 from src.Processor import Processor
 
 adult = "./datasets/adult/adult.data"
+aheader = ['age','workclass','fnlwgt','education', 'education-num', 'marital-status','occupation','relationship','race', 'sex','capital-gain','capital-loss','hours-per-week','native-country', 'salary']
 
-aReader = Processor(adult)
+aReader = Processor(adult, aheader)
 
-for index, row in aReader.raw.head().iterrows():
-    print(row[0])
+
+# remove last col
+X = aReader.raw.iloc[:, :-1] 
+# print(X['native-country']
+
+# print(X.drop("?", axis=0).shape)
--- Project1/src/Processor.py
+++ Project1/src/Processor.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 
 class Processor:
-    def __init__(self, filepath):
+    def __init__(self, filepath, header):
         self.path = filepath
-        self.raw = self.__read(self.path)
+        self.raw = self.__read(self.path, header)
 
     def clean(self):
         pass
@@ -12,8 +12,8 @@ class Processor:
     def analyze(self):
         pass
 
-    def __read(self, path):
-        return pd.read_csv(path)
+    def __read(self, path, header):
+        return pd.read_csv(path, names=header, header=None)
 
     def __str__(self):
      return self.raw.head().to_string()
