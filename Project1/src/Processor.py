a8b30937d5d0c5032a1a7bb7e545a216994f3fb3
Omar Ibrahim <oielbanna@gmail.com>
2020-01-31 16:27:20 -0500

    stage 1 processing class done

--- Project1/src/Processor.py
+++ Project1/src/Processor.py
@@ -2,39 +2,35 @@
 import pandas as pd
 
 class Processor:
-    def __init__(self, filepath, header, headerTypes):
-        if(len(header) != len(headerTypes)):
-            raise Exception('Header array and headerTypes array should be the same length')
-        self.path = filepath
-        self.header = header
-        self.types = headerTypes
-        self.raw = self.__read(self.path, header)
-        self.data = []
-
     # removing all rows with '?'
-    def removeMissing(self):
-        X = self.data
-        if(len(self.data) == 0):
-            X = self.raw
+    @staticmethod
+    def removeMissing(X):
+        # for i, head in enumerate(self.header):
+        #     if(self.types[i] == str):
+        #          X = X[~X[head].str.contains("\?")]
+        return X.dropna(axis=0)
 
-        for i, head in enumerate(self.header):
-            if(self.types[i] == str):
-                 X = X[~X[head].str.contains("\?")]
+    @staticmethod
+    def fillMissing(X):
+        raise Exception("Method unimplemented.")
+        obj_df = obj_df.fillna({"num_doors": "four"})
 
-        self.data = X
-        return self.data
+    @staticmethod
+    def toBinaryCol(X, dic):
+        X.replace(dic, inplace=True)
+        return X
 
-    def fillMissing(self):
-        pass
+    # One Hot Encoding for all columns with type object (ie categorical cols)
+    @staticmethod
+    def OHE(X):
+        cols = list(X.select_dtypes(include=['object']).columns)
+        return pd.get_dummies(X, columns=cols)
 
-    def toBinaryCol(self, name):
-        pass
-
-    def analyze(self):
-        pass
-
-    def __read(self, path, header):
-        return pd.read_csv(path, names=header, header=None)
-
-    def __str__(self):
-     return self.raw.head().to_string()
+    @staticmethod
+    def analyze(X):
+        return X.describe()
+    
+    @staticmethod
+    def read(path, header):
+        return pd.read_csv(path,
+                  header=None, names=header, na_values="?",skipinitialspace=True)
