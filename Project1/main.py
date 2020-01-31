178be0a607e09c04f4a2761798b6525e9d1c2425
Omar Ibrahim <oielbanna@gmail.com>
2020-01-31 14:33:44 -0500

    remove missing rows

--- Project1/main.py
+++ Project1/main.py
@@ -1,13 +1,31 @@
 from src.Processor import Processor
+import numpy as np
+import pandas as pd
 
 adult = "./datasets/adult/adult.data"
 aheader = ['age','workclass','fnlwgt','education', 'education-num', 'marital-status','occupation','relationship','race', 'sex','capital-gain','capital-loss','hours-per-week','native-country', 'salary']
-
-aReader = Processor(adult, aheader)
-
+atypes = [np.int64, np.str, np.uint64, np.str, np.uint64, np.str, np.str, np.str, np.str, np.str, np.uint64, np.uint64, np.uint64, np.str, np.str]
+aReader = Processor(adult, aheader, atypes)
 
 # remove last col
-X = aReader.raw.iloc[:, :-1] 
-# print(X['native-country']
+# X = aReader.raw.iloc[:, :-1] 
+X = aReader.raw
+# for i in aheader:
+#     print(i)
+#     X = X[X[i] != '?']
+# for i, row in X.iterrows():
+#     for j, col in row.iteritems():
+#         # print(type(col))
+#         if(type(col) is str and "?" in col):
+#             # print(row)
+#             print(i)
+#             # X.drop(axis=0, index=i)
+#             break
 
-# print(X.drop("?", axis=0).shape)
+aReader.removeMissing()
+print(aReader.data)
+# dt = np.dtype({'names':aheader, 'formats':atypes})
+# X = np.loadtxt(adult,delimiter=", ", dtype=dt)
+
+# df = pd.DataFrame(X)
+# print(df.dtypes)
--- Project1/src/Processor.py
+++ Project1/src/Processor.py
@@ -2,11 +2,32 @@ import numpy as np
 import pandas as pd
 
 class Processor:
-    def __init__(self, filepath, header):
+    def __init__(self, filepath, header, headerTypes):
+        if(len(header) != len(headerTypes)):
+            raise Exception('Header array and headerTypes array should be the same length')
         self.path = filepath
+        self.header = header
+        self.types = headerTypes
         self.raw = self.__read(self.path, header)
+        self.data = []
 
-    def clean(self):
+    # removing all rows with '?'
+    def removeMissing(self):
+        X = self.data
+        if(len(self.data) == 0):
+            X = self.raw
+
+        for i, head in enumerate(self.header):
+            if(self.types[i] == np.str):
+                 X = X[~X[head].str.contains("\?")]
+
+        self.data = X
+        return self.data
+
+    def fillMissing(self):
+        pass
+
+    def toBinaryCol(self, name):
         pass
 
     def analyze(self):
