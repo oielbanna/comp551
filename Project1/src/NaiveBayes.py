1011980190909378b57bdc58eb20ab84892f963f
Justin Sun <justin.sun@mail.mcgill.ca>
2020-02-05 15:06:39 -0500

    initial commit

--- Project1/src/NaiveBayes.py
+++ Project1/src/NaiveBayes.py
@@ -10,6 +10,7 @@ def posterior(x_test, x_train_split, x, mean, std):
     post = np.prod(likelihood, axis=1) * (x_train_split.shape[0] / x.shape[0])
     return post
 
+
 class NaiveBayes:
 
     def __init__(self):
