3233899932efd0ef807b22e4ee9b6d7d971339b1
Omar Ibrahim <oielbanna@gmail.com>
2020-02-08 11:44:18 -0500

    moved cleanup out of main

--- Project1/src/main.py
+++ Project1/src/main.py
@@ -1,7 +1,8 @@
 from Project1.src.LogisticRegression import LogisticRegression
 from Project1.src.Processor import Processor
+from Project1.src.Clean import Clean
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def evaluate_acc(true_labels, predicted, verbose=False):
     """
@@ -38,8 +39,6 @@ def evaluate_acc(true_labels, predicted, verbose=False):
 
     return accuracy
 
-import matplotlib.pyplot as plt
-
 adult = True
 
 if adult:
@@ -47,37 +46,14 @@ if adult:
     header = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation',
               'relationship',
               'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'salary']
-    binaryCols = {
-        "sex": {"Male": 0, "Female": 1},
-        "salary": {">50K": 0, "<=50K": 1}
-    }
-    X = Processor.read(path, header)
-    X = Processor.removeMissing(X)
-    X = Processor.toBinaryCol(X, binaryCols)
 
-    # print(X['native-country'].value_counts())
+    All = Processor.read(path, header)
 
-    X = Processor.normalize(X, ["fnlwgt", "hours-per-week"])
-    Y = X["salary"]
-    X = X.iloc[:, :-1]
-    X = Processor.OHE(X)
-
-    countryCols = ["native-country_Cambodia", "native-country_England", "native-country_Puerto-Rico", "native-country_Canada", "native-country_Outlying-US(Guam-USVI-etc)", "native-country_India", "native-country_Japan", "native-country_Greece", "native-country_South", "native-country_China", "native-country_Cuba", "native-country_Iran", "native-country_Honduras", "native-country_Italy", "native-country_Poland", "native-country_Jamaica", "native-country_Vietnam", "native-country_Portugal", "native-country_Ireland", "native-country_France", "native-country_Dominican-Republic", "native-country_Laos", "native-country_Ecuador", "native-country_Taiwan", "native-country_Haiti", "native-country_Columbia", "native-country_Hungary", "native-country_Guatemala", "native-country_Nicaragua", "native-country_Scotland", "native-country_Thailand", "native-country_Yugoslavia", "native-country_El-Salvador", "native-country_Trinadad&Tobago", "native-country_Peru", "native-country_Hong", "native-country_Holand-Netherlands"]
-
-    X = X.drop(columns=(["capital-gain", "capital-loss", "education-num"] + countryCols))
+    [X, Y] = Clean.adult(All)
 
     print(X.shape)
-    # X['hours-per-week'].value_counts().plot(x='Age', linestyle="None", marker='o')
 
-    # X['native-country'].hist(grid=False)
-
-    # X.plot.scatter(x="relationship", y="age",  c='salary', colormap='autumn')
-
-
-
-    # plt.waitforbuttonpress()
-
-    [X_train, X_test, Y_train, Y_test] = Processor.split(X, Y, train=0.85)
+    [X_train, X_test, Y_train, Y_test] = Processor.split(X, Y, train=0.8)
 
     model = LogisticRegression()
     w = model.fit(X_train.to_numpy(), Processor.ToNumpyCol(Y_train), max_gradient=0.8, learning_rate=0.05)
@@ -85,39 +61,20 @@ if adult:
     print("DONE TRAINING")
     print(evaluate_acc(Processor.ToNumpyCol(Y_test), model.predict(X_test.to_numpy())))
 
-
-    # TODO frequency distribution of each feature
-
-    # TODO see the correlation between features (pairs) in a scatter plot
-    # important features will be seperated
-
 else:
     path = "./datasets/ionosphere/ionosphere.data"
     header = ["{}{}".format("col", x) for x in range(33 + 1)]
     header.append("signal")
-    binaryCols = {
-        "signal": {"g": 1, "b": 0}
-    }
 
-    X = Processor.read(path, header)
-    X = Processor.removeMissing(X)
-    X = Processor.toBinaryCol(X, binaryCols)
-    Y = X["signal"]
-    X = X.iloc[:, :-1]
-    [X_train, X_test, Y_train, Y_test] = Processor.split(X, Y, train=0.1)
+    All = Processor.read(path, header)
 
-    Y_train = Y_train.to_numpy()
-    Y_train = Y_train.reshape((Y_train.shape[0], 1))
+    [X, Y] = Clean.Ionosphere(All)
+    [X_train, X_test, Y_train, Y_test] = Processor.split(X, Y, train=0.8)
 
     model = LogisticRegression()
-    w = model.fit(X_train.to_numpy(), Y_train)
+    w = model.fit(X_train.to_numpy(), Processor.ToNumpyCol(Y_train))
 
-    print(model.predict(X_test.to_numpy()))
-    # print(Y_test)
-
-    Y_test = Y_test.to_numpy()
-    Y_test = Y_test.reshape((Y_test.shape[0], 1))
-    print(evaluate_acc(Y_test, model.predict(X_test.to_numpy()), verbose=True))
+    print(evaluate_acc(Processor.ToNumpyCol(Y_test), model.predict(X_test.to_numpy()), verbose=True))
 
 
 
