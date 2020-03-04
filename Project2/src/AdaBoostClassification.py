498953ab93eec1aa7c1ce248a4f7dfac63d3c58d
mohamed-youssef-bou <mohamedyoussefbouaouina@gmail.com>
2020-03-04 11:45:52 -0500

    Updating final test script for AdaBoost

--- Project2/src/AdaBoostClassification.py
+++ Project2/src/AdaBoostClassification.py
@@ -1,10 +1,13 @@
+"""
+This script is used for the final evaluation of the AdaBoost model.
+The hyper parameters used were chosen using the crossvalidation script: AdaBoostCV.py
+"""
 import numpy as np
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import Normalizer
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import accuracy_score
-
 
 # Get data and convert to numpy array when needed
 print('Fetching data...')
@@ -13,7 +16,6 @@ X_test, y_test = fetch_20newsgroups(subset='test', remove=('headers', 'footers',
 
 X_train = np.array(X_train)
 X_test = np.array(X_test)
-
 
 # Pre-process the text data by applying tf-idf vectorization and normalizing
 print('Vectorizing data...')
@@ -28,7 +30,7 @@ norm_vect_test = normalizer.transform(vect_test)
 
 # Instantiate model, train, and get predictions on test set
 print('Training model...')
-clf = AdaBoostClassifier(n_estimators=100, random_state=0)
+clf = AdaBoostClassifier(n_estimators=50, random_state=0)
 clf.fit(norm_vect_train, y_train)
 
 print('Predicting...')
@@ -37,8 +39,3 @@ y_hat = clf.predict(norm_vect_test)
 # Evaluate the model
 print('Accuracy score on the training set ' + str(clf.score(norm_vect_train, y_train)))
 print('Accuracy score on the testing set ' + str(accuracy_score(y_test, y_hat)))
-
-
-
-
-
