8598967c5a604551553ec1c5c5838043f036aa5d
mohamed-youssef-bou <mohamedyoussefbouaouina@gmail.com>
2020-03-07 09:42:53 -0500

    Customized vectorizer and updated AdaBoost scripts

--- Project2/src/AdaBoostCV.py
+++ Project2/src/AdaBoostCV.py
@@ -1,23 +1,15 @@
 import numpy as np
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import Normalizer
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import GridSearchCV
+from Project2.src.Cleaner import Cleaner
 
 # Get data and convert to numpy array when needed
 print('Fetching data...')
 X_train, y_train = fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'), return_X_y=True)
 X_train = np.array(X_train)
 
-# Pre-process the text data by applying tf-idf vectorization and normalizing
-print('Vectorizing data...')
-vectorizer = TfidfVectorizer()
-vect_train = vectorizer.fit_transform(X_train)
-
-print('Normalizing data...')
-normalizer = Normalizer().fit(X=vect_train)
-norm_vect_train = normalizer.transform(vect_train)
+norm_vect_train = Cleaner.newsgroups(X_train, subset='train', verbose=True)
 
 tuned_parameters = [{'n_estimators': [125, 175, 200, 225]}]
 
--- Project2/src/Cleaner.py
+++ Project2/src/Cleaner.py
@@ -1,26 +1,55 @@
+import string
 import numpy as np
-
+import nltk
+from nltk import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import Normalizer
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+nltk.download('punkt')
+
+# Update stopwords list with punctuation
+sw = stopwords.words('english') + list(string.punctuation)
+
+stemmer = SnowballStemmer('english')
+
+
+def custom_tokenizer(doc):
+    tokens = word_tokenize(doc)
+    return [stemmer.stem(token) for token in tokens]
+
+
+vectorizer = TfidfVectorizer(strip_accents='ascii', lowercase=True,
+                             tokenizer=custom_tokenizer,
+                             analyzer='word', stop_words=sw,
+                             ngram_range=(1, 2))
+
+normalizer = Normalizer()
 
 
 class Cleaner:
+
     @staticmethod
-    def newsgroups(X, y, verbose=False):
+    def newsgroups(X, subset, verbose=False):
         X_train = np.array(X)
 
-        # Pre-process the text data by applying tf-idf vectorization and normalizing
         if verbose:
-            print('Vectorizing data...')
-        vectorizer = TfidfVectorizer()
-        vect_train = vectorizer.fit_transform(X_train)
+            print('Vectorizing {} data...'.format(subset))
+
+        if subset == 'train':
+            vect_train = vectorizer.fit_transform(X_train)
+        elif subset == 'test':
+            vect_train = vectorizer.transform(X_train)
+        else:
+            raise ValueError
 
         if verbose:
-            print('Normalizing data...')
-        normalizer = Normalizer()
-        norm_vect_train = normalizer.fit_transform(vect_train)
+            print('Normalizing {} data...'.format(subset))
 
-        return norm_vect_train, y
+        norm_vect_train = normalizer.transform(vect_train)
+
+        return norm_vect_train
 
     @staticmethod
     def IMDB(X, y, verbose=False):
