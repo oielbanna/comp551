b7a7584b7dff6d3d69dc723476399d8857da5a7f
mohamed-youssef-bou <mohamedyoussefbouaouina@gmail.com>
2020-03-08 09:59:21 -0400

    Added results of the MergeIMDB script

--- Project2/datasets/MergeIMDB.py
+++ Project2/datasets/MergeIMDB.py
@@ -1,25 +1,34 @@
+"""
+Instead of uploading the whole dataset to GitHub with the data separated in different files,
+this script was used to merge the data from the data files into one csv for each subset (train/test) while tagging the
+reviews (positive 1 or negative -1)
+"""
 import glob
 import pandas as pd
 
-extension = 'txt'
+# Pick which subset to merge its data
+subset = 'test'
 
 # Combine training data into one csv file
-
-for file in glob.iglob('imdb_data\\train\\pos\\*.txt'):
+pos_reviews = []
+for file in glob.iglob('imdb_data\\{}\\pos\\*.txt'.format(subset)):
     with open(file, encoding='utf8') as file_content:
         review = file_content.read().replace('\n', '')
-        combined_pos = pd.concat([pd.DataFrame([review])])
+        pos_reviews.append([review])
 
-combined_pos['target'] = 1  # pos files so set the target to 1
-combined_pos.to_csv('train_reviews.csv', index=False)
+df = pd.DataFrame(pos_reviews, columns=['reviews'])
+df['target'] = 1  # pos files so set the target to 1
+df.to_csv('{}_reviews.csv'.format(subset), index=False)
 print("Done with pos reviews")
 
-for file in glob.iglob('imdb_data\\train\\neg\\*.txt'):
+neg_reviews = []
+for file in glob.iglob('imdb_data\\{}\\neg\\*.txt'.format(subset)):
     with open(file, encoding='utf8') as file_content:
         review = file_content.read().replace('\n', '')
-        combined_neg = pd.concat([pd.DataFrame([review])])
+        neg_reviews.append([review])
 
-combined_neg['target'] = -1  # neg files so set the target to -1
-combined_neg.to_csv('train_reviews.csv', index=False, mode='a')
+df = pd.DataFrame(neg_reviews, columns=['reviews'])
+df['target'] = -1  # neg files so set the target to -1
+df.to_csv('{}_reviews.csv'.format(subset), index=False, header=False, mode='a')
 print("Done with neg reviews")
 
