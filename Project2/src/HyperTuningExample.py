a89874c2b2a4bd29aa6b4f6bef944b6e94b95394
mohamed-youssef-bou <mohamedyoussefbouaouina@gmail.com>
2020-03-12 18:08:49 -0400

    Getting ready for submission

--- Project2/src/HyperTuningExample.py
+++ Project2/src/HyperTuningExample.py
@@ -1,13 +1,11 @@
+"""
+This script is an example of how we tune hyperparameters by using the GridSearchCV model from Sklearn.
+"""
 import numpy as np
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import GridSearchCV
-import string
-import numpy as np
-
-
-
-
+from Project2.src.Cleaner import Cleaner
 
 # Get data and convert to numpy array when needed
 print('Fetching data...')
@@ -16,15 +14,14 @@ X_train = np.array(X_train)
 
 norm_vect_train = Cleaner.clean(X_train, subset='train', verbose=True)
 
-tuned_parameters = [{'n_neighbors': [25, 50, 100, 150],
-                     'weights': ['uniform', 'distance'],
-                     'p': [1, 2]}]
+tuned_parameters = [{'n_estimators': [125, 175, 200, 225]}]
 
 # 5-fold cross validation using an AdaBoost clf with fixed params
 print('Cross-validating...')
-clf = KNeighborsClassifier()
+clf = AdaBoostClassifier(learning_rate=0.8, random_state=0)
 clf = GridSearchCV(clf, tuned_parameters, cv=5, refit=False, verbose=3)
 clf.fit(norm_vect_train, y_train)
 scores = clf.cv_results_['mean_test_score'].round(3)
 
 print('scores:', scores)
+
