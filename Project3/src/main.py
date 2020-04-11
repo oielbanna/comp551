9a8b4cf6c15426237628ca1cc6521193b80ee08f
Justin Sun <justin.sun@mail.mcgill.ca>
2020-04-11 11:42:56 -0700

    relu and sigmoid derivatives

---
+++ Project3/src/main.py
@@ -0,0 +1,25 @@
+from Project3.src.mlp import MLP
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+mlp = MLP([2, 3, 2], 'relu')
+# print(mlp.weights[0])
