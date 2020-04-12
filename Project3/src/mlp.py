7a6b65b1f1d9d92099612acce336bba3d00169d0
Omar Ibrahim <oielbanna@gmail.com>
2020-04-12 14:13:10 -0400

    no message

--- Project3/src/mlp.py
+++ Project3/src/mlp.py
@@ -47,13 +47,34 @@ class MLP:
 
         self.weights = self.initialize_weights()
 
-    def train(self, X, Y, epochs=200):
+    def train(self, X, Y, batch_size=10, epochs=1000, lr=.01):
+        """
+        params_values = init_layers(nn_architecture, 2)
+        cost_history = []
+        accuracy_history = []
+
+        for i in range(epochs):
+            Y_hat, cashe = full_forward_propagation(X, params_values, nn_architecture)
+            cost = get_cost_value(Y_hat, Y)
+            cost_history.append(cost)
+            accuracy = get_accuracy_value(Y_hat, Y)
+            accuracy_history.append(accuracy)
+
+            grads_values = full_backward_propagation(Y_hat, Y, cashe, params_values, nn_architecture)
+            params_values = update(params_values, grads_values, nn_architecture, learning_rate)
+
+        return params_values, cost_history, accuracy_history
+        """
+
         for epoch in range(epochs):
-            gradients = self.backpropogation(X, Y)
+            # for our code, we do the fullforward and cost inside the backpropogation right away.
+            gradients = self.backward(X, Y, learning_rate=lr, batch_size=batch_size)
+
+            # Todo: need to update those weights after doing backpropagation but use a better function.
             self.weights = np.subtract(self.weights, gradients)
 
     def predict(self, X):
-        A, Z = self.feedforward(X)
+        Z, A = self.feedforward(X)
         return A[-1]
 
     def initialize_weights(self):
@@ -73,36 +94,18 @@ class MLP:
         randoms = np.random.randint(X.shape[0], size=int(n))
         return X[randoms], Y[randoms]
 
-    def GD(self, X, Y, M, learning_rate=.1, eps=1e-9, max_iters=100000):
-        N, D = X.shape
-        N, K = Y.shape
-
-        # make it ndarray right away!
-        W = np.random.randn(M, K) * .01
-        V = np.random.randn(D, M) * .01
-        dW = np.inf * np.ones_like(W)
-        t = 0
-        while np.linalg.norm(dW) > eps and t < max_iters:
-            # batch the data radomly first
-            X_train, Y_train = self.batch(X, Y)
-
-            # do forward here, break up gradients function
-            dW, dV = self.gradients(X, Y, W, V)
-
-            # last part of back propogation
-            W = W - learning_rate * dW
-            V = V - learning_rate * dV
-            t += 1
-        return W, V
-
     '''Just the first couple lines that were in the gradient function
         All we need here is Z (output of out first layer dot weights corresponding to that layer)
         and Yh (output) which is the Z dot W (weights of last layer)'''
     def feedforward(self, X):
+        """
+        :param X: Input dataset
+        :return: all the activations as an ndarray, Yhat (prediction)
+        """
         activations = []
         inputs = X
         for layer in range(self.n_layers - 1):
-            if layer == self.n_layers -1:
+            if layer == self.n_layers - 1:
                 A, Z = self.single_forward_propagation(inputs, self.weights[layer], softmax)
             else:
                 A, Z = self.single_forward_propagation(inputs, self.weights[layer], relu)
@@ -120,18 +123,18 @@ class MLP:
     
     Questions: why feed forward at every iteration?? Its doing that with regular GD as well. When GD calls gradient() at every iteration gradient is running feedforward
     WHYYYYYYYYYY AHHHHHHHHH, '''
-    def backward(self, X, Y, W, V, M, learning_rate=.1, eps=1e-9, max_iters=100000):
+    def backward(self, X, Y, learning_rate=.1, eps=1e-9, max_iters=100000, batch_size=0.3):
         N, D = X.shape
         t = 0
         while np.linalg.norm(dW) > eps and t < max_iters:
-            # batch the data radomly first
-            X_batch, Y_batch = self.batch(X, Y)
+            # batch the data radomly
+            X_batch, Y_batch = self.batch(X, Y, batch_size=batch_size)
 
             # do forward here, break up gradients function
             # cost should be computed in the backprop stage
-            Z, Yh = self.forward(X_batch, W, V)
+            Z, Yh = self.feedforward(X_batch)
 
-            dY = self.ssd(Yh, Y)  # cost
+            dY = self.SSD(Yh, Y_batch)  # cost
 
             # Something happens after
             dW = np.dot(Z.T, dY) / N  # M x K
@@ -147,31 +150,31 @@ class MLP:
         return W, V
 
 
-    def ssd(self, Yh, Y):
+    def SSD(self, Yh, Y):
         return np.sum((Yh - Y) ** 2)
 
 
-    def gradients(self,
-                  X,  # N x D
-                  Y,  # N x K
-                  W,  # M x K
-                  V,  # D x M
-                  ):
-        # forwards
-        Z = self.logistic(np.dot(X, V))  # N x M
-        N, D = X.shape
-        Yh = softmax(np.dot(Z, W))  # N x K
-
-        # cost function, lets use a better cost function!
-        dY = Yh - Y  # N x K
-
-        # Something happen after
-        dW = np.dot(Z.T, dY) / N  # M x K
-        dZ = np.dot(dY, W.T)  # N x M
-
-        # calculate derivative
-        dV = np.dot(X.T, dZ * Z * (1 - Z)) / N  # D x M
-        return dW, dV
+    # def gradients(self,
+    #               X,  # N x D
+    #               Y,  # N x K
+    #               W,  # M x K
+    #               V,  # D x M
+    #               ):
+    #     # forwards
+    #     Z = self.logistic(np.dot(X, V))  # N x M
+    #     N, D = X.shape
+    #     Yh = softmax(np.dot(Z, W))  # N x K
+    #
+    #     # cost function, lets use a better cost function!
+    #     dY = Yh - Y  # N x K
+    #
+    #     # Something happen after
+    #     dW = np.dot(Z.T, dY) / N  # M x K
+    #     dZ = np.dot(dY, W.T)  # N x M
+    #
+    #     # calculate derivative
+    #     dV = np.dot(X.T, dZ * Z * (1 - Z)) / N  # D x M
+    #     return dW, dV
 
     # def feedforward(self, x):
     #     input_layer = x
@@ -201,27 +204,49 @@ class MLP:
     #     a[self.n_layers - 1] = output_layer
     #     return a, z
 
-    def backpropagation(self, x, y):
-        n_examples = x.shape[0]
-        a, z = self.feedforward(y)
+    # def backpropagation(self, x, y):
+    #     n_examples = x.shape[0]
+    #     a, z = self.feedforward(y)
+    #
+    #     # backpropagation, calculate weights based on the softmax function (last layer)
+    #     updates = self.n_layers * [None]
+    #     updates[-1] = a[-1] - y
+    #
+    #     # update weights from the second last layer to the second layer
+    #     for layer in np.arrange(self.n_layers - 2, 0, -1):
+    #         if self.activation_func == 'sigmoid':
+    #             updates[layer] = (np.dot(self.weights[layer].T, updates[layer + 1].T).T) * sigmoid_derivative(z[layer])
+    #         elif self.activation_func == 'relu':
+    #             updates[layer] = (np.dot(self.weights[layer].T, updates[layer + 1].T).T) * relu_derivative(z[layer])
+    #
+    #     # compute gradient
+    #     gradients = (self.n_layer - 1) * [None]
+    #     for layer in range(self.n_layers - 1):
+    #         temp_grads = np.dot(updates[layer + 1].T, a[layer]) / n_examples
+    #         temp_grads = temp_grads + (self.lambda_r / n_examples) * self.weights[layer][:, 1:]
+    #
+    #         gradients[layer] = temp_grads
+    #
+    #     return gradients
 
-        # backpropagation, calculate weights based on the softmax function (last layer)
-        updates = self.n_layers * [None]
-        updates[-1] = a[-1] - y
-
-        # update weights from the second last layer to the second layer
-        for layer in np.arrange(self.n_layers - 2, 0, -1):
-            if self.activation_func == 'sigmoid':
-                updates[layer] = (np.dot(self.weights[layer].T, updates[layer + 1].T).T) * sigmoid_derivative(z[layer])
-            elif self.activation_func == 'relu':
-                updates[layer] = (np.dot(self.weights[layer].T, updates[layer + 1].T).T) * relu_derivative(z[layer])
-
-        # compute gradient
-        gradients = (self.n_layer - 1) * [None]
-        for layer in range(self.n_layers - 1):
-            temp_grads = np.dot(updates[layer + 1].T, a[layer]) / n_examples
-            temp_grads = temp_grads + (self.lambda_r / n_examples) * self.weights[layer][:, 1:]
-
-            gradients[layer] = temp_grads
-
-        return gradients
+    # def GD(self, X, Y, M, learning_rate=.1, eps=1e-9, max_iters=100000):
+    #     N, D = X.shape
+    #     N, K = Y.shape
+    #
+    #     # make it ndarray right away!
+    #     W = np.random.randn(M, K) * .01
+    #     V = np.random.randn(D, M) * .01
+    #     dW = np.inf * np.ones_like(W)
+    #     t = 0
+    #     while np.linalg.norm(dW) > eps and t < max_iters:
+    #         # batch the data radomly first
+    #         X_train, Y_train = self.batch(X, Y)
+    #
+    #         # do forward here, break up gradients function
+    #         dW, dV = self.gradients(X, Y, W, V)
+    #
+    #         # last part of back propogation
+    #         W = W - learning_rate * dW
+    #         V = V - learning_rate * dV
+    #         t += 1
+    #     return W, V
