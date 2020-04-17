0b39c07b204fc0f61a8a63bf44c30be1617933b6
Omar Ibrahim <oielbanna@gmail.com>
2020-04-16 21:14:32 -0400

    better results 75

--- Project3/src/Full.py
+++ Project3/src/Full.py
@@ -6,9 +6,6 @@ from tensorflow.keras import datasets
 train_images = zscore(train_images, axis=None)
 test_images = zscore(test_images, axis=None)
 
-X = train_images
-Y = train_labels
-
 
 def OHV(vector, size=10):
     """
@@ -37,6 +34,15 @@ class NN2(object):
     def sigmoid(self, z):
         return 1.0 / (1.0 + np.exp(-z + 1e-10))
 
+    def feedforward(self, X, W, V):
+        hidden = np.dot(X, V)
+        Z = self.sigmoid(hidden)  # N x M
+
+        output = np.dot(Z, W)
+        Yh = self.softmax(output)  # N x K
+
+        return [hidden, output], [Z, Yh]
+
     def cost(self,
              X,  # N x D
              Y,  # N x C
@@ -46,7 +52,7 @@ class NN2(object):
         Q = np.dot(X, V)  # N x M
         Z = self.sigmoid(Q)  # N x M
         U = np.dot(Z, W)  # N x K
-        print(X.shape, U)
+        # print(X.shape, U)
         Yh = self.softmax(U)
         nll = - np.mean(np.sum(U * Y, 1) - self.logsumexp(U))
         return nll, Yh
@@ -58,19 +64,21 @@ class NN2(object):
                   V,  # D x M
                   ):
         # feedforward
-        Z = self.sigmoid(np.dot(X, V))  # N x M
-        N, D = X.shape
-        output = np.dot(Z, W)
-        Yh = self.softmax(output)  # N x K
+        # Z = self.sigmoid(np.dot(X, V))  # N x M
+        # output = np.dot(Z, W)
+        # Yh = self.softmax(output)  # N x K
+        outs, activations = self.feedforward(X, W, V);
 
         # cost
-        dY = Yh - Y  # N x K
+        dY = activations[-1] - Y  # N x K
+        cost = self.cost(X, Y, W, V)
 
+        N, D = X.shape
         # backpropagation
-        dW = np.dot(Z.T, dY) / N  # M x K
+        dW = np.dot(activations[0].T, dY) / N  # M x K
         dZ = np.dot(dY, W.T)  # N x M
-        dV = np.dot(X.T, dZ * Z * (1 - Z)) / N  # D x M
-        return dW, dV
+        dV = np.dot(X.T, dZ * activations[0] * (1 - activations[0])) / N  # D x M
+        return dW, dV, cost
 
     def logsumexp(
             self,
@@ -80,48 +88,45 @@ class NN2(object):
         lse = Zmax + np.log(np.sum(np.exp(Z - Zmax), axis=1))[:, None]
         return lse  # N
 
-    # def softmax(
-    #         self,
-    #         u,  # N x C
-    # ):
-    #     u_exp = np.exp(u - np.max(u)[:, None])
-    #     return u_exp / np.sum(u_exp, axis=-1)[:, None]
-
     def softmax(self, u):
         u_exp = np.exp(u - np.max(u))
         return u_exp / np.sum(u_exp)
 
-    def GD(self, X, Y, M, lr=.1, eps=1e-9, max_iters=10000):
+    def train(self, X, Y, M, lr=.1, eps=1e-9, epochs=1, max_iters=10000):
         print(X.shape)
         N, D = X.shape
         N, K = Y.shape
-        W = np.random.randn(M, K) * .01
-        V = np.random.randn(D, M) * .01
-        dW = np.inf * np.ones_like(W)
-        t = 0
-        while np.linalg.norm(dW) > eps and t < max_iters:
+        cost_aggregate = []
+        for epoch in range(epochs):
+            W = np.random.randn(M, K) * .01
+            V = np.random.randn(D, M) * .01
+            dW = np.inf * np.ones_like(W)
+            t = 0
             x_batch, y_batch = batch(X, Y)
-            # for x, y in zip(x_batch, y_batch):
-            dW, dV = self.gradients(x_batch, y_batch, W, V)
-            W = W - lr * dW
-            V = V - lr * dV
-            t += 1
+            while np.linalg.norm(dW) > eps and t < max_iters:
+                dW, dV, cost = self.gradients(x_batch, y_batch, W, V)
+                W = W - lr * dW
+                V = V - lr * dV
+                t += 1
+                # print(type(cost))
+                # cost_aggregate.append(cost)
+            # print("Epoch {} with loss {}".format(epoch, np.average(cost_aggregate)))
         return W, V
 
 
+def preprocess(X, Y):
+    X = np.asarray([x.flatten() for x in X])
+    Y = np.asarray([OHV(y) for y in Y])
+    return X, Y
+
+
 NN = NN2()
-X = np.asarray([x.flatten() for x in X])
-Y = np.asarray([OHV(y) for y in Y])
-W, V = NN.GD(X, Y, 100)
+X, Y = preprocess(train_images, train_labels)
+
+W, V = NN.train(X, Y, 100)
 
 # print(W, V)
 # print(X[100:300].shape)
 _, Yhat = NN.cost(X[300:500], Y[300: 500], W, V)
-# print(Yhat.shape)
-# for img, label in zip(train_images[1:50], train_labels[1:50]):  # testing on 50 images
-#     o = NN.predict(img)
-#     Yhat = np.append(Yhat, o)
-#     # print("Predicted Output: " + str(o))
-#     # print("Actual Output: " + str(label))
 
 NN.eval(Yhat, Y[300:500])
