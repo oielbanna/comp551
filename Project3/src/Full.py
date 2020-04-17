37ff82402a51ca1e821eae9cf2164085bef3ce90
Omar Ibrahim <oielbanna@gmail.com>
2020-04-17 19:56:48 -0400

    no message

--- Project3/src/Full.py
+++ Project3/src/Full.py
@@ -1,6 +1,7 @@
 from scipy.stats import zscore
 import numpy as np
 from tensorflow.keras import datasets
+import time
 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 train_images = zscore(train_images, axis=None)
@@ -24,76 +25,78 @@ def batch(X, Y, batch_size=100):
     return X[randoms], Y[randoms]
 
 
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z + 1e-10))
+
+
+def eval(Yhat, Ytrue):
+    Yhat, Ytrue = Yhat.flatten(), Ytrue.flatten()
+    count = np.sum(Yhat == Ytrue)
+    print("Accuracy: %f" % ((float(count) / Yhat.shape[0]) * 100))
+
+
+def softmax(u):
+    u_exp = np.exp(u - np.max(u))
+    return u_exp / np.sum(u_exp)
+
+
+def logsumexp(
+        Z,  # NxC
+):
+    Zmax = np.max(Z, axis=1)[:, None]
+    lse = Zmax + np.log(np.sum(np.exp(Z - Zmax), axis=1))[:, None]
+    return lse  # N
+
+
+def cross_entropy_cost(out2, Y):
+    nll = - np.mean(np.sum(out2 * Y, 1) - logsumexp(out2))
+    return nll
+
+
 class NN2(object):
-
-    def eval(self, Yhat, Ytrue):
-        Yhat, Ytrue = Yhat.flatten(), Ytrue.flatten()
-        count = np.sum(Yhat == Ytrue)
-        print("Accuracy: %f" % ((float(count) / Yhat.shape[0]) * 100))
-
-    def sigmoid(self, z):
-        return 1.0 / (1.0 + np.exp(-z + 1e-10))
 
     def feedforward(self, X, W, V):
         hidden = np.dot(X, V)
-        Z = self.sigmoid(hidden)  # N x M
+        Z = sigmoid(hidden)  # N x M
 
         output = np.dot(Z, W)
-        Yh = self.softmax(output)  # N x K
+        Yh = softmax(output)  # N x K
 
         return [hidden, output], [Z, Yh]
 
-    def cost(self,
-             X,  # N x D
-             Y,  # N x C
-             W,  # M x C
-             V,  # D x M
-             ):
-        Q = np.dot(X, V)  # N x M
-        Z = self.sigmoid(Q)  # N x M
-        U = np.dot(Z, W)  # N x K
-        # print(X.shape, U)
-        Yh = self.softmax(U)
-        nll = - np.mean(np.sum(U * Y, 1) - self.logsumexp(U))
-        return nll, Yh
-
-    def gradients(self,
-                  X,  # N x D
-                  Y,  # N x K
-                  W,  # M x K
-                  V,  # D x M
-                  ):
-        # feedforward
-        # Z = self.sigmoid(np.dot(X, V))  # N x M
-        # output = np.dot(Z, W)
-        # Yh = self.softmax(output)  # N x K
+    def backpropagation(self,
+                        X,  # N x D
+                        Y,  # N x K
+                        W,  # M x K
+                        V,  # D x M
+                        ):
         outs, activations = self.feedforward(X, W, V);
 
         # cost
         dY = activations[-1] - Y  # N x K
-        cost = self.cost(X, Y, W, V)
+        cost = cross_entropy_cost(outs[1], Y)
 
-        N, D = X.shape
         # backpropagation
+        N, D = X.shape
         dW = np.dot(activations[0].T, dY) / N  # M x K
         dZ = np.dot(dY, W.T)  # N x M
         dV = np.dot(X.T, dZ * activations[0] * (1 - activations[0])) / N  # D x M
         return dW, dV, cost
 
-    def logsumexp(
-            self,
-            Z,  # NxC
-    ):
-        Zmax = np.max(Z, axis=1)[:, None]
-        lse = Zmax + np.log(np.sum(np.exp(Z - Zmax), axis=1))[:, None]
-        return lse  # N
-
-    def softmax(self, u):
-        u_exp = np.exp(u - np.max(u))
-        return u_exp / np.sum(u_exp)
-
-    def train(self, X, Y, M, lr=.1, eps=1e-9, epochs=1, max_iters=10000):
-        print(X.shape)
+    def train(self, X, Y, M, lr=.1, eps=1e-9, epochs=4, batch_size=200, max_iters=10000, verbose=False):
+        """
+        :param X: Dataset
+        :param Y: One hot vector of prediction
+        :param M: size of hidden layer
+        :param lr: learning rate of gradient descent
+        :param eps
+        :param epochs
+        :param batch_size
+        :param max_iters
+        :param verbose
+        :return:
+        """
+        # print(X.shape)
         N, D = X.shape
         N, K = Y.shape
         cost_aggregate = []
@@ -102,16 +105,21 @@ class NN2(object):
             V = np.random.randn(D, M) * .01
             dW = np.inf * np.ones_like(W)
             t = 0
-            x_batch, y_batch = batch(X, Y)
+            x_batch, y_batch = batch(X, Y, batch_size)
             while np.linalg.norm(dW) > eps and t < max_iters:
-                dW, dV, cost = self.gradients(x_batch, y_batch, W, V)
+                dW, dV, cost = self.backpropagation(x_batch, y_batch, W, V)
                 W = W - lr * dW
                 V = V - lr * dV
                 t += 1
                 # print(type(cost))
-                # cost_aggregate.append(cost)
-            # print("Epoch {} with loss {}".format(epoch, np.average(cost_aggregate)))
+                cost_aggregate.append(cost)
+            if verbose:
+                print("Epoch {} with loss {}".format(epoch, np.average(cost_aggregate)))
         return W, V
+
+    def predict(self, X, W, V):
+        _, activations = self.feedforward(X, W, V)
+        return activations[-1]
 
 
 def preprocess(X, Y):
@@ -122,11 +130,17 @@ def preprocess(X, Y):
 
 NN = NN2()
 X, Y = preprocess(train_images, train_labels)
+print("Training...")
 
-W, V = NN.train(X, Y, 100)
+start = time.time()
+W, V = NN.train(X, Y, 100, verbose=True)
+end = time.time()
+print(end - start)
 
 # print(W, V)
 # print(X[100:300].shape)
-_, Yhat = NN.cost(X[300:500], Y[300: 500], W, V)
+X_test, Y_test = preprocess(test_images, test_labels)
+print("Predicting...")
+Yhat = NN.predict(X_test, W, V)
 
-NN.eval(Yhat, Y[300:500])
+eval(Yhat, Y_test)
